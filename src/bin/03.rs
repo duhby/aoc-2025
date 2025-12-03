4d30459152cd843684e18ebf8951cf448c0650ed
Josh <epikmemez@outlook.com>
2025-12-03 00:30:04 -0700

    draft

---
+++ src/bin/03.rs
@@ -0,0 +1,89 @@
+advent_of_code::solution!(3);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let banks: Vec<&str> = input.split('\n').collect();
+    let mut highest: Vec<i32> = vec![];
+    for bank in banks {
+        if bank == "" {
+            continue;
+        }
+        let chars: Vec<char> = bank.chars().collect();
+        let mut highest_1 = (0, 0);
+        for (i, c) in chars[0..chars.len() - 1].iter().enumerate() {
+            let num: i32 = c.to_string().parse().unwrap();
+            if num > highest_1.1 {
+                highest_1 = (i, num);
+            }
+        }
+        let mut highest_2 = 0;
+        for c in &chars[highest_1.0 + 1..chars.len()] {
+            let num: i32 = c.to_string().parse().unwrap();
+            if num > highest_2 {
+                highest_2 = num;
+            }
+        }
+        highest.push(highest_1.1 * 10 + highest_2);
+    }
+
+    Some(highest.iter().sum::<i32>() as u64)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let banks: Vec<&str> = input.split('\n').collect();
+    let mut highest: Vec<u64> = vec![];
+    let mut i = 0;
+    for bank in banks {
+        if i == 0 {
+            i += 1;
+            continue;
+        }
+        if bank == "" {
+            continue;
+        }
+        let chars: Vec<char> = bank.chars().collect();
+        let mut arr = vec![vec!["".to_string(); chars.len()]; 12];
+        for i in 0..12 {
+            for j in (0..chars.len()).rev() {
+                let max_str_w = if i != 0 {
+                    if j == chars.len() - 1 {
+                        ""
+                    } else {
+                        arr[i - 1][j + 1..chars.len()].iter().max().unwrap()
+                    }
+                } else {
+                    ""
+                };
+                let max_str_wo: u64 = arr[i][j..chars.len()]
+                    .iter()
+                    .max()
+                    .unwrap()
+                    .parse()
+                    .unwrap_or(0);
+                let max_str_w: u64 = format!("{}{}", chars[j], max_str_w).parse().unwrap();
+                arr[i][j] = max_str_wo.max(max_str_w).to_string();
+            }
+        }
+        dbg!(&arr);
+        highest.push((arr[11][0]).parse().unwrap());
+        break;
+    }
+
+    Some(highest.iter().sum::<u64>())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(357));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(17229));
+    }
+}
